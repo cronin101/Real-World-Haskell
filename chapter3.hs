e2607ec23b30271dbb9eb4085c0809d50137e15d
Aaron Cronin <cronin@include.cat>
2013-08-12 15:21:33 +0100

    HLint ran on chapter 3

--- chapter3.hs
+++ chapter3.hs
@@ -21,7 +21,7 @@ data Magazine = Magazine {
 } deriving (Show)
 
 
-a_book = Book 01 "Some Book" ["Some", "Lines", "Of", "Text"]
+aBook = Book 01 "Some Book" ["Some", "Lines", "Of", "Text"]
 
 type CustomerID = Int
 
@@ -38,13 +38,13 @@ data MagazineReview = MagazineReview {
 } deriving (Show)
 
 
-a_book_review = BookReview a_book 01 ["This book was quite good."]
+aBookReview = BookReview aBook 01 ["This book was quite good."]
 
 data BookRecord     = BookRecord Book BookReview
   deriving (Show)
 data MagazineRecord = MagazineRecord Magazine MagazineReview
 
-a_book_record = BookRecord a_book a_book_review
+aBookRecord = BookRecord aBook aBookReview
 
 type CardHolder = String
 type CardNumber = String
@@ -68,12 +68,11 @@ data List a = Cons a (List a)
             | Nil
   deriving (Show)
 
-fromList (x:xs) = Cons x (fromList xs)
-fromList []     = Nil
+fromList = foldr Cons Nil
 
 --Write the converse of fromList for the List type:
 --  a function that takes a List a and generates a [a].
-toList (Cons x (xs)) = x : (toList xs)
+toList (Cons x xs) = x : toList xs
 toList Nil           = []
 
 data Tree a = Node a (Tree a) (Tree a)
@@ -84,8 +83,8 @@ data Tree a = Node a (Tree a) (Tree a)
 --Instead of the Empty constructor, use the Maybe type to refer to a node's children.
 data MTree a = MNode {
   mTreeNode       :: a,
-  leftMTreeChild  :: (Maybe (MTree a)),
-  rightMTreeChild :: (Maybe (MTree a))
+  leftMTreeChild  :: Maybe (MTree a),
+  rightMTreeChild :: Maybe (MTree a)
 } deriving (Show)
 
 tidySafeSecond :: [a] -> Maybe a
@@ -93,21 +92,20 @@ tidySafeSecond (_:x:_) = Just x
 tidySafeSecond _       = Nothing
 
 --Write a function that computes the number of elements in a list.
-myLength []     = 0
-myLength (x:xs) = 1 + myLength xs
+myLength = foldr (const (+ 1)) 0
 
 --Add a type signature for your function to your source file.
 myLength :: (Num i) => [a] -> i
 
 --Write a function that computes the mean of a list.
 myMean :: [Int] -> Double
-myMean l    = sum / (fromIntegral $ length l)
-  where sum = (foldr (+) 0 $ map fromIntegral l)
+myMean l    = total / fromIntegral (length l)
+  where total = sum $ map fromIntegral l
 
 --Turn a list into a palindrome, i.e. it should read the same both backwards and forwards.
 --For example, given the list [1,2,3], your function should return [1,2,3,3,2,1].
 myPalindrome :: [a] -> [a]
-myPalindrome l = l ++ (reverse l)
+myPalindrome l = l ++ reverse l
 
 --Create a function that sorts a list of lists based on the length of each sublist.
 mySortByLength :: [[a]] -> [[a]]
@@ -117,7 +115,7 @@ mySortByLength = L.sortBy (O.comparing length)
 intersperse :: a -> [[a]] -> [a]
 intersperse sep (x:xs)
   | null xs   = x
-  | otherwise = x ++ (sep : (intersperse sep xs))
+  | otherwise = x ++ sep : intersperse sep xs
 
 --Write a function that will determine the height of a Tree.
 treeHeight :: Tree a -> Int
@@ -149,17 +147,17 @@ calculateDirection a b c
     where
       crossProductDirection   = ((x2 - x1) * (y3 - y1)) - ((y2 - y1) * (x3 - x1))
         where
-          x1                  = pX(a)
-          x2                  = pX(b)
-          x3                  = pX(c)
-          y1                  = pY(a)
-          y2                  = pY(b)
-          y3                  = pY(c)
+          x1                  = pX a
+          x2                  = pX b
+          x3                  = pX c
+          y1                  = pY a
+          y2                  = pY b
+          y3                  = pY c
 
 --Define a function that takes a list of 2D points and computes the direction of each successive triple.
 tripleDirections :: [Point] -> [Direction]
 tripleDirections (a:b:c:[])   = [calculateDirection a b c]
-tripleDirections (a:b:c:rest) = (calculateDirection a b c) : tripleDirections (b:c:rest)
+tripleDirections (a:b:c:rest) = calculateDirection a b c : tripleDirections (b:c:rest)
 
 --Using the code from the preceding three exercises, implement Graham's scan algorithm for the convex hull of a set of 2D points.
 grahamScan :: [Point] -> [Point]
@@ -169,20 +167,20 @@ grahamScan points = firstPoint : scan sortedPoints
       | isLeftTurn a b c = [b]
       | otherwise        = []
     scan (a:b:c:rest)
-      | isLeftTurn a b c = (b : (scan (b:c:rest)))
+      | isLeftTurn a b c = b : scan (b:c:rest)
       | otherwise        = scan (a:c:rest)
-    isLeftTurn a b c     = (calculateDirection a b c) == LeftTurn
+    isLeftTurn a b c     = calculateDirection a b c == LeftTurn
     firstPoint           = head pointsByYValue
     pointsByYValue       = L.sortBy (O.comparing pY) $ L.sortBy (O.comparing pX) points
-    sortedPoints         = firstPoint : (reverse $ L.sortBy (O.comparing polarAngle) (tail pointsByYValue))
+    sortedPoints         = firstPoint : L.sortBy (flip (O.comparing polarAngle)) (tail pointsByYValue)
       where
         polarAngle point   = angleBetween firstPoint (Point (pX point) 0) point
         angleBetween a b c = acos $ top / bottom
           where
             top                          = fromIntegral $ dotProduct ba bc
-            bottom                       = magnitude(ba) * magnitude(bc)
+            bottom                       = magnitude ba * magnitude bc
             dotProduct (x1, y1) (x2, y2) = (x1 * x2) + (y1 * y2)
             magnitude (x, y)             = sqrt $ fromIntegral $ (x ^ 2) + (y ^ 2)
-            ba                           = (pX(a) - pX(b), pY(a) - pY(b))
-            bc                           = (pX(c) - pX(b), pY(c) - pY(b))
+            ba                           = (pX a - pX b, pY a - pY b)
+            bc                           = (pX c - pX b, pY c - pY b)
 
