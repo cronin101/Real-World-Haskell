9ce6aec4bd2da9da9f93ca84ab06643a54ed192c
Aaron Cronin <cronin@include.cat>
2013-08-15 16:19:32 +0100

    Chapter 4: Safe and unsafe string2int parsing completed.

--- chapter4/chapter4.hs
+++ chapter4/chapter4.hs
@@ -1,3 +1,6 @@
+import qualified Data.Char as C
+import qualified Data.List as L
+
 --Write your own “safe” definitions of the standard partial list functions,
 --but make sure that yours never fail.
 applyUnlessNull :: ([a] -> b) -> [a] -> Maybe b
@@ -28,3 +31,27 @@ splitWith predicate xs = splitHelper predicate xs
       | null suf  = [pre]
       | otherwise = pre : splitHelper predicate (tail suf)
         where (pre, suf) = break predicate xs
+
+--Use a fold (choosing the appropriate fold will make your code much
+--  simpler) to rewrite and improve upon the asInt function from the section
+--  called “Explicit recursion”.
+asIntFold :: String -> Int
+asIntFold all@(x:xs)
+  | x == '-'  = negate $ foldToInt xs
+  | otherwise = foldToInt all
+    where
+      foldToInt = L.foldl' ((. C.digitToInt) . (+). (10 *)) 0
+
+--The asInt_fold function uses error, so its callers cannot handle errors.
+--Rewrite it to fix this problem.
+type ErrorMessage = String
+asIntEither :: String -> Either ErrorMessage Int
+asIntEither all@(x:xs)
+  | x == '-'  = fmap negate $ safeFoldToInt xs
+  | otherwise = safeFoldToInt all
+    where
+      safeFoldToInt = L.foldl' processChar $ Right 0
+        where
+          processChar acc x
+            | C.isDigit x = fmap ((C.digitToInt x +) . (10 *)) acc
+            | otherwise   = Left $ "Non-digit '" ++ [x] ++ "' detected"
